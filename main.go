e5785c1809ac16bcebc20633c01326ef5acde3de
psimoes <p.simoes@campus.fct.unl.pt>
2017-11-25 19:45:17 +0000

    set function almost complete

--- main.go
+++ main.go
@@ -1,67 +1,134 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
 
-  "github.com/urfave/cli"
+	"github.com/urfave/cli"
 )
 
-func IsRoot() bool{
-	return os.Geteuid()==0
+var brightness = "/sys/class/backlight/intel_backlight/brightness"
+var maxbrightness = "/sys/class/backlight/intel_backlight/max_brightness"
+
+//IsRoot checks if user is running command as root
+func IsRoot() bool {
+	return os.Geteuid() == 0
 }
 
-func Inc(c *cli.Context)error {
-	if !IsRoot(){
+//Inc function opens brightness file, reads, and increments by 50, if possible
+func Inc(c *cli.Context) error {
+	if !IsRoot() {
+		fmt.Println("Got root?")
 		return fmt.Errorf("this tool needs root access")
 	}
 	fmt.Println("increase")
 	return nil
 }
-func Dec(c *cli.Context)error {
-	if !IsRoot(){
+
+//Dec function opens brightness file, reads, and decrements by 50, if possible
+func Dec(c *cli.Context) error {
+	if !IsRoot() {
+		fmt.Println("Got root?")
 		return fmt.Errorf("this tool needs root access")
 	}
 	fmt.Println("decrease")
-	return nil	
-}
-func Set(c *cli.Context)error {
-	if c.NArg() > 0 {
-		name := c.Args()[0:]
-	fmt.Println(name)
-    }
-
 	return nil
 }
+
+//Set write to brightness file a value corresponding to the percentage given by the user
+func Set(c *cli.Context) error {
+
+	var (
+		err        error
+		percentage int
+		max        int
+		value      int
+	)
+
+	if c.NArg() > 0 {
+
+		arg := c.Args()[0]
+
+		percentage, err = strconv.Atoi(arg)
+
+		if err != nil {
+
+			fmt.Println("Percentage has to be an integer")
+
+			return fmt.Errorf("Percentage has to be an integer")
+		}
+
+		switch {
+
+		case percentage < 20 || percentage > 100:
+
+			fmt.Println("Percentage has to be between 20 and 100")
+
+			return fmt.Errorf("Percentage has to be between 20 and 100")
+
+		default:
+			max, err = ReadFile(maxbrightness)
+
+			if err != nil {
+				fmt.Println("failed to find max brightness")
+
+				return fmt.Errorf("failed to find max brightness")
+
+			}
+			value = ((max - 100) * percentage) / 100
+			fmt.Println(value)
+		}
+	}
+
+	return WriteFile(value)
+}
+
+//ReadFile reads a file containing only integers
+func ReadFile(filename string) (int, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return -1, err
+	}
+
+	return strconv.Atoi(string(content))
+}
+
+//WriteFile writes an integer to brightness file
+func WriteFile(n int) error {
+
+	return nil
+
+}
+
 func main() {
 
-  app := cli.NewApp()
-  app.Name = "mac_air_brightness"
-  app.Usage = "change screen brightness of your mac book air!"
+	app := cli.NewApp()
+	app.Name = "mac_air_brightness"
+	app.Usage = "change screen brightness of your mac book air!"
 
-
-   
- app.Commands = []cli.Command{
-    {
-      Name:    "set",
-      Aliases: []string{"s"},
-      Usage:   "set brightness manually",
-      Action:  Set,
-    },
-    {
-      Name:    "inc",
-      Aliases: []string{"i"},
-      Usage:   "increase brightness",
-      Action:  Inc,
-  },
-	 {
-      Name:    "dec",
-      Aliases: []string{"d"},
-      Usage:   "decrease brightness",
-      Action:  Dec,
-    },
-  }
-  app.Version="0.0.1"
-  app.Author="Seomis : psimoes@campus.fct.unl.pt"
-  app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:    "set",
+			Aliases: []string{"s"},
+			Usage:   "set brightness manually",
+			Action:  Set,
+		},
+		{
+			Name:    "inc",
+			Aliases: []string{"i"},
+			Usage:   "increase brightness",
+			Action:  Inc,
+		},
+		{
+			Name:    "dec",
+			Aliases: []string{"d"},
+			Usage:   "decrease brightness",
+			Action:  Dec,
+		},
+	}
+	app.Version = "0.0.1"
+	app.Author = "Seomis : psimoes@campus.fct.unl.pt"
+	app.Run(os.Args)
 }
